57008d31915f5383d168be82facb36a185d8a713
Zliu53 <ZLiu53@slb.com>
2023-11-09 16:36:48 +0800

    Refactor: Counter-Group list use input and output to set counter.num

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
+  @Input()
  Number:number =0;
  public onDescrease()
  {
