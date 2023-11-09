122f3bdba096f3aea080d5d202e85c2c8edb6101
Zliu53 <ZLiu53@slb.com>
2023-11-09 16:32:05 +0800

    Feat&Test: Counter-Group

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -18,4 +18,20 @@ describe('CounterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should Increase', () => {
+    component.Number=5;
+
+    component.onIncrease();
+    
+    expect(component.Number).toEqual(6);
+  });
+
+  it('should Decrease', () => {
+    component.Number=5;
+
+    component.onDescrease();
+
+    expect(component.Number).toEqual(4);
+  });
 });
