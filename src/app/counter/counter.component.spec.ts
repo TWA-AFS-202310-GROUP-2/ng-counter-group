2ec5c2b1ff54a179debff710b7e8bb3f572e3b62
Tju-Bibibo <34788685+Tju-Bibibo@users.noreply.github.com>
2023-11-09 18:46:53 +0800

    fix: css style

--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SimpleChanges } from '@angular/core';
 
 import { CounterComponent } from './counter.component';
 import { By } from '@angular/platform-browser';
@@ -52,6 +53,38 @@ describe('CounterComponent', () => {
     component.resetCounter();
     // then
     expect(component.currentCount).toEqual(0);
+  });
+
+  it('should update currentCount when ngOnChanges is called with new value', () => {
+    // given
+    const newCount = 5;
+    const changesObj: SimpleChanges = {
+      currentCount: {
+        previousValue: component.currentCount,
+        currentValue: newCount,
+        firstChange: false,
+        isFirstChange: () => false
+      }
+    };
+
+    // when
+    component.ngOnChanges(changesObj);
+
+    // then
+    expect(component.currentCount).toBe(newCount);
+  });
+
+  it('should emit removeCounterEvent with currentCount when removeCounter is called', () => {
+    // given
+    spyOn(component.removeCounterEvent, 'emit'); // Spy on the emit method
+    const initialCount = 3;
+    component.currentCount = initialCount;
+
+    // when
+    component.removeCounter();
+
+    // then
+    expect(component.removeCounterEvent.emit).toHaveBeenCalledWith(initialCount);
   });
 
 });
